e9a5db2ca338eb950dddf001bee693bd24a4b056
Ari Makela <hauva@iki.fi>
2012-07-16 20:30:41 +0300

    The program compiles.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+///
+/// A C++ Chess Library
+/// 
+/// Copyright (c) Ari Makela 2012
+///
+/// See file COPYRIGHT.
+///
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  return EXIT_SUCCESS;
+}
+
+
