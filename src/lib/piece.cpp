daf8c52979eecee27f12d686a21cf0e41a9cf74b
Ari Makela <hauva@iki.fi>
2012-07-17 13:50:52 +0300

    first commit

---
+++ src/lib/piece.cpp
@@ -0,0 +1,12 @@
+#include "constants.hpp"
+#include "piece.hpp"
+
+#include <cassert>
+#include <iostream>
+
+namespace Acl {
+	Piece::Piece(int color) : color_(color), symbols_("KQRBNP") {
+		assert((color == WHITE) || (color == BLACK));
+	}
+}
+
