95e0d65ff9d0e21f0bb4d643a2be897abf69d54d
Ari Makela <hauva@iki.fi>
2012-07-19 01:46:35 +0300

    Doxygen /// comments.

--- src/lib/piece.hpp
+++ src/lib/piece.hpp
@@ -10,63 +10,74 @@ namespace Acl {
   protected:
     const int color_;
     char *symbols_;
+
   public:
-    /**
-       @param WHITE or BLACK	
-    */
+
+    ///
+    /// @param WHITE or BLACK	
+    ///
+
     Piece(int color);
     virtual ~Piece() {}
 
-    /**
-       @return The piece as a character symbol.
-    */
+    ///
+    /// @return The piece as a character symbol.
+    ///
+
     virtual char chr() const = 0;
 
-    /**
-       @return true if the piece is a pawn.
-    */
+    ///
+    /// @return true if the piece is a pawn.
+    ///
+
     virtual bool isPawn() const { 
       return false; 
     }
 
-    /**
-       @return true if the piece is a knight.
-    */
+    ///
+    /// @return true if the piece is a knight.
+    ///
+
     virtual bool isKnight() const { 
       return false; 
     }
 
-    /**
-       @return true if the piece is a bishop.
-    */
+    ///
+    /// @return true if the piece is a bishop.
+    ///
+
     virtual bool isBishop() const { 
       return false; 
     }
 
-    /**
-       @return true if the piece is a rook.
-    */
+    ///
+    /// @return true if the piece is a rook.
+    ///
+
     virtual bool isRook() const { 
       return false; 
     }
 
-    /**
-       @return true if the piece is a queen.
-    */
+    ///
+    /// @return true if the piece is a queen.
+    ///
+
     virtual bool isQueen() const { 
       return false; 
     }
 
-    /**
-       @return true if the piece is a king.
-    */
+    /// 
+    /// @return true if the piece is a king.
+    ///
+
     virtual bool isKing() const { 
       return false; 
     }
 			
-    /**
-       @return true if the piece is white.
-    */
+    ///
+    /// @return true if the piece is white.
+    ///
+
     inline bool isWhite() const {
       if (color_ == WHITE)
 	return true;
@@ -74,9 +85,10 @@ namespace Acl {
 	return false;
     }
 			
-    /**
-       @return true if the piece is black.
-    */
+    ///
+    /// @return true if the piece is black.
+    ///
+
     inline bool isBlack() const {
       if (color_ == BLACK)
 	return true;
@@ -85,11 +97,9 @@ namespace Acl {
     }
 
 
-    /**
-
-       @return WHITE or BLACK.
-			
-    */
+    ///
+    /// @return WHITE or BLACK.
+    ///
 
     inline int getColor() const {
       return color_;
