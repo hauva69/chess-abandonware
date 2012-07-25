2255fd5f360ff95bd1cf85ab742b6cd02a4a20f7
Ari Makela <hauva@iki.fi>
2012-07-25 12:06:07 +0300

    first commit

---
+++ bin/chess.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+'''
+GUI for the C++ Chess Library.
+
+AUTHOR
+
+Ari Makela <hauva@iki.fi>
+'''
+
+def main():
+    print('hello, world')
+
+if __name__ == '__main__':
+    main()
