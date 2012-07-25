4061ae057cbce17fdb8d6b60e4ab6ec96904cdee
Ari Makela <hauva@iki.fi>
2012-07-25 18:19:44 +0300

    Used now WebView to show the Github page.

--- python/chess/__init__.py
+++ python/chess/__init__.py
@@ -8,8 +8,22 @@ AUTHOR
 Ari Makela <hauva@iki.fi>
 '''
 
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtWebKit import *
+
+import sys
+
+URL = 'https://github.com/hauva69/chess'
+
 def main():
-    print('hello, world')
+    app = QApplication(sys.argv)
+    web = QWebView()
+    web.load(QUrl(URL))
+    web.show()
+
+    sys.exit(app.exec_())
+    
 
 if __name__ == '__main__':
     main()
