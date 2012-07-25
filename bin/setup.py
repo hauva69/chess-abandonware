e11dc77e9d34f12e79a7cff13a818c82418d67eb
Ari Makela <hauva@iki.fi>
2012-07-25 12:34:19 +0300

    implemented setup

---
+++ bin/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+    name='chess',
+    author='Ari Makela',
+    author_email='hauva@iki.fi',
+    url='https://github.com/hauva69/chess',
+    version='0.01dev',
+    packages=['chess',],
+    license='GPLv3',
+    long_description=open('README').read(),
+)
